35cfbfd427c4b48c9fd2ec8f32fd637a1c0cc600
Camillle Nieto <nietocamille06@gmail.com>
2023-03-10 09:02:44 +0800

    prototype 4
    .gitignore
    .gitattributes

---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        //i will set to 0, then add to 1 so i = 1
+        for (int i = 0; i < enemiesToSpawn; i = i += 1)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    //void functions use specific task that doesn't return any value
+    private Vector3 GenerateSpawnPosition()
+    {
+        //position where enemies spawn at field
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+        //returns vector 3
+        //return data from that method
+    }
+}
